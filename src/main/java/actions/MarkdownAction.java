3867a92ca7e3c70e8e715e6e139903bdd6fb9107
Irem Ugurlu <Ugurlu@student.tudelft.nl>
2020-04-11 15:22:45 +0200

    Add more statistics and split links as url / file reference

--- src/main/java/actions/MarkdownAction.java
+++ src/main/java/actions/MarkdownAction.java
@@ -19,14 +19,22 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import static org.intellij.plugins.markdown.lang.MarkdownElementTypes.*;
 import static org.intellij.markdown.flavours.gfm.GFMTokenTypes.*;
+import static org.intellij.plugins.markdown.lang.MarkdownTokenTypes.ATX_HEADER;
+import static org.intellij.plugins.markdown.lang.MarkdownTokenTypes.TEXT;
+
 import com.intellij.openapi.diagnostic.Logger;
 
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+/**
+ * Class for the report of markdown files statistics summary
+ */
 public class MarkdownAction extends AnAction {
 
-    private static final Logger log = Logger.getInstance(MarkdownAction.class);
+    private static final Logger log = Logger.getInstance(MarkdownAction.class); //logger for debugging
 
     /**
      * This default constructor is used by the IntelliJ Platform framework to
@@ -50,13 +58,20 @@ public class MarkdownAction extends AnAction {
         super(text, description, icon);
     }
 
+    // variables to store
+    // TODO : find a better way to store these numbers
     int noOfLinks = 0;
     int noOfFiles = 0;
     int noOfFIlesWithLinks = 0;
+    int noOfRepoLinks = 0;
+    int noOfUrls = 0;
+    int noOfLines = 0;
+    int noOfHeaders = 0;
     boolean linkFound = false;
+    List<String> repoReferences = new ArrayList<>();
+    List<String> urls = new ArrayList<>();
 
     /**
-     * TODO deneme
      * @param event
      */
     @Override
@@ -69,15 +84,29 @@ public class MarkdownAction extends AnAction {
         noOfLinks = 0;
         noOfFiles = 0;
         noOfFIlesWithLinks = 0;
+        noOfRepoLinks = 0;
+        noOfUrls = 0;
+        noOfLines = 0;
+        noOfHeaders = 0;
+        repoReferences = new ArrayList<>();
+        urls = new ArrayList<>();
 
+        // gets each md file and gathers statistics
         for (VirtualFile virtualFile : virtualFiles) {
+
             linkFound = false;
             noOfFiles++;
+
             MarkdownFile psiFile = (MarkdownFile)PsiManager.getInstance(currentProject).findFile(virtualFile);
+
+            noOfLines += psiFile.getText().split("\r\n|\r|\n").length;
+
             psiFile.accept(new PsiRecursiveElementVisitor() {
                 @Override
                 public void visitElement(PsiElement element) {
                     IElementType elemType = element.getNode().getElementType();
+
+                    //finds links by looking at right element types in a markdown file
                     if ((element.getClass().equals(ASTWrapperPsiElement.class) && elemType == AUTOLINK)
                             || (element.getClass().equals(MarkdownLinkDestinationImpl.class) && elemType == LINK_DESTINATION)
                             || (element.getClass().equals(LeafPsiElement.class)
@@ -85,16 +114,51 @@ public class MarkdownAction extends AnAction {
                             && element.getParent().getNode().getElementType() != LINK_DESTINATION))) {
                         linkFound = true;
                         noOfLinks++;
+
+                        if((element.getClass().equals(MarkdownLinkDestinationImpl.class) && elemType == LINK_DESTINATION)) {
+                            if(element.getFirstChild().getNode().getElementType()==TEXT){
+                                repoReferences.add(element.getText());
+                                noOfRepoLinks++;
+                            } else {
+                                urls.add(element.getText());
+                                noOfUrls++;
+                            }
+                        } else {
+                            urls.add(element.getText());
+                            noOfUrls++;
+                        }
+
                     }
+
+                    // counts headers
+                    if(elemType == ATX_HEADER) {
+                        noOfHeaders++;
+                    }
+
                     super.visitElement(element);
                 }
             });
+
+            // counts numbers of files with links
             if (linkFound) {
                 noOfFIlesWithLinks++;
             }
         }
 
-        String msg = "No of links : " + noOfLinks + "\nNo of files : " + noOfFiles + "\nNo of files with links : " + noOfFIlesWithLinks;
+        String msg = "No of markdown files : " + noOfFiles + "\nTotal no of lines: " + noOfLines +
+                "\nNo of headers : " + noOfHeaders + "\nNo of files with links : " + noOfFIlesWithLinks +
+                "\nTotal no of links : " + noOfLinks +"\nNo of reference links to repo : " + noOfRepoLinks +
+                "\nNo of urls : " + noOfUrls;
+
+        msg+="\nRepo references:";
+        for(String ref : repoReferences) {
+            msg+="\n-" + ref;
+        }
+
+        msg+="\nUrl links:";
+        for(String url : urls) {
+            msg+="\n-" + url;
+        }
 
         Messages.showMessageDialog(currentProject, msg, "Markdown Files Report", Messages.getInformationIcon());
     }
