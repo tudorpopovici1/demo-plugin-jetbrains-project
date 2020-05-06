50af665808009fd0aef9ad6bab6fc9ccb2e6659d
Tudor <t.popovici@student.tudelft.nl>
2020-05-06 03:38:34 +0300

    ok12

---
+++ src/main/java/actions/GitTracker.java
@@ -0,0 +1,24 @@
+/*package service;
+
+import com.intellij.openapi.options.Configurable;
+import com.intellij.openapi.vcs.AbstractVcs;
+import org.jetbrains.annotations.NotNull;
+
+public class GitTracker extends AbstractVcs {
+
+    public GitTracker() {
+        super();
+    }
+
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public Configurable getConfigurable() {
+        return null;
+    }
+}*/
